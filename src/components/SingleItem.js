ad417263cb4dd5ab7cf562df6cd7155038d03ed5
Colin Forbes <cjforbes32@gmail.com>
2022-03-19 22:10:38 -0400

    database can now be queried for the proper item from the SingleItem view

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
+import { useParams } from 'react-router-dom';
 
-const SingleItem = (props) => {
+const SingleItem = () => {
+  const params = useParams();
   const [loading, setLoading] = useState(true);
-  const { item, setItem } = useState(null);
-  const { owner, setOwner } = useState(null);
+  const [item, setItem] = useState(null);
+  const [owner, setOwner] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    // console.log(params);
+    getItem();
+  }, []);
 
-  const getItem = async () => {};
+  const getItem = async () => {
+    try {
+      setLoading(true);
+      let { data, error, status } = await supabase
+        .from('items')
+        .select()
+        .eq('id', params.id);
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (data) {
+        setItem(data);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return <div>{loading ? <p>Loading</p> : <div>Hello World</div>}</div>;
+  return <div>{loading ? <p>Loading</p> : <div>{console.log(item)}</div>}</div>;
 };
 
 export default SingleItem;
