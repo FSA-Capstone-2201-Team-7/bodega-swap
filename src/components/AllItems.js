bddd534207297b07094088814183c0966d8be05d
Colin Forbes <cjforbes32@gmail.com>
2022-03-19 20:10:37 -0400

    AllItems component scaffolding written. database query written. testing to follow

---
+++ src/components/AllItems.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { supabase } from '../supabaseClient';
+import { useEffect } from 'react/cjs/react.production.min';
+
+const AllItems = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const getItems = async () => {
+    try {
+      setLoading(true);
+      let { data, error, status } = await supabase.from('items').select();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        console.log(data);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return <div>{loading ? <p>Loading</p> : <p>hello</p>}</div>;
+};
