bac374641937a781d429f4546c4e3743aff7753e
Brennan Skinner <bgskinner3@gmail.com>
2022-03-20 22:53:12 -0400

    restructured haggleview to only pull haggle from database between specific user and haggler

--- src/components/HaggleView.js
+++ src/components/HaggleView.js
@@ -2,98 +2,72 @@ import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import { useLocation } from 'react-router-dom';
 
+const HaggleView = ({ state }) => {
+  const [loading, setLoading] = useState(true);
+  const [barter, setBarter] = useState(null);
+  const location = useLocation();
+  const { item = '' } = location.state || {};
+  const user = supabase.auth.user();
+
+  useEffect(() => {
+    const getAllBarters = async () => {
+      try {
+        setLoading(true);
+        const { data, error } = await supabase
+          .from('barters')
+          .select(
+            `
+            hagglerid,
+            ownerId,
+            id,
+            hagglerWants,
+            ownerWants,
+            status
+            `
+          )
+          .eq('hagglerid', user.id)
+          .eq('ownerId', item.userId);
+        setBarter(data);
+
+        console.log('users', data);
+      } catch (error) {
+        console.error('try agina', error);
+      }
+    };
+    getAllBarters();
+  }, [user, item]);
+  console.log('barter', barter);
+  useEffect(() => {
+    const makeBarter = async () => {
+      try {
+        if (!barter.length) {
+          const { data: newBarter} = await supabase
+            .from('barters')
+            .insert([
+              {
+                hagglerid: user.id,
+                status: 'pending',
+                ownerId: item.userId,
+                hagglerWants: [item.id],
+              },
+            ]);
+          setBarter(newBarter);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    makeBarter()
+  }, []);
 
 
-const HaggleView = ({state}) => {
-     const [loading, setLoading] = useState(true);
-      const location = useLocation();
-    
-   const {item = ''} = location.state || {}
-
-const user = supabase.auth.user();
-console.log(item)
-///create a new barter 
-// useEffect(() => {
-//   const barter = async () => {
-//     const { data, error } = await supabase.from('barters').insert([
-//       {
-//         hagglerid: user.id,
-//         status: 'pending',
-//         ownerId: item.userId,
-//         hagglerWants: [item.id]
-
-//       },
-//     ]);
-    
-//   };
-//   barter()
-// }, [user, item])
+  return <div>Haggle</div>;
+};
+export default HaggleView;
 
 
 
-  // useEffect(() => {
-  //     const getBarter = async () => {
-  //       try {
-  //         setLoading(true);
-  //         const { data, error } = await supabase
-  //           .from('barters')
-  //           .select()
-  //           .eq('hagglerid', user.id);
-  //           // .match({ hagglerid: user.id });
-  //         if (data) {
-  //           setSwap(data[0]);
-  //         }
-  //       } catch (error) {
-  //         console.error('try agina', error);
-  //       } finally {
-  //         setLoading(false);
-  //       }
-  //     };
-  //     getBarter()
-  // }, [user.id])
-  // console.log(swapInfo)
 
-  return <div>hello</div>;
-}
-export default HaggleView
-
-// import React, { useState, useEffect } from 'react';
-// import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
-// import { supabase } from '../supabaseClient';
-// import { useParams } from 'react-router';
-// import { Card, Image, Form, Button, Container } from 'react-bootstrap';
-
-// const HaggleView = ({item}) => {
-
-//   console.log(item)
-
-//    const [loading, setLoading] = useState(true);
-//    const [swapInfo, setSwap] = useState({});
-// //   const [ownerWantsItems, setOwnerWants] = useState();
-// //   const [hagglerWantsItems, setHagglerWants] = useState();
-
-//   useEffect(() => {
-//     getBarter();
-
-//   }, [item]);
-
-// //   // will move to differe component then pass swapinfo through params
-//   const getBarter = async () => {
-//     try {
-//       setLoading(true);
-//       const { data, error } = await supabase
-//         .from('barters')
-//         .select()
-//         .match({ id: 1 });
-//       if (data) {
-//         setSwap(data[0]);
-//       }
-//     } catch (error) {
-//       console.error('try agina', error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
 
 //   return loading ? (
 //     <p>Loading</p>
@@ -140,124 +114,5 @@ export default HaggleView
 // };
 // export default HaggleView;
 
-// let items = [];
-// const getItemsOwner = async () => {
-//   try {
-//     setLoading(true);
-//     if(ownerWants) {
-//     ownerWants.map(async (el) => {
-//       const { data, error } = await supabase
-//         .from('items')
-//         .select()
-//         .match({ id: el });
-//       if(data) {
-//        setOwnerWants([
-//  ...ownerWantsItems
-//  data[0]
-//]);
-
-//       }
-//        setOwnerWants(items);
-
-//     });
-//   }
-
-//   } catch (error) {
-//     console.error('try agina', error);
-//   } finally {
-//     setLoading(false);
-//   }
-
-// };
-// let items2 = []
-// const getItemsHaggler = async () => {
-//   try {
-//     setLoading(true);
-//     hagglerWants.map(async (el) => {
-//       const { data, error } = await supabase
-//         .from('items')
-//         .select()
-//         .match({ id: el });
-//       if (data) {
-//         items2.push(data[0]);
-//       }
-//     });
-//     setHagglerWants(items2)
-//   } catch (error) {
-//     console.error('try agina', error);
-//   } finally {
-//     setLoading(false);
-//   }
-// }
-
-// console.log('haggleritems', hagglerWantsItems);
-//console.log('owner Items', ownerWantsItems);
-// const { singleItem } = getSingleItems();
-// console.log(ownerWantsItems);
-
-//   const { ownerWants } = swapInfo;
-
-//   const getItemsOwner = () => {
-//     try {
-//       setLoading(true);
-
-//        ownerWants.map(async (el) => {
-//         const { data, error } = await supabase
-//           .from('items')
-//           .select(`*, users(username)`)
-//           .eq('id', params.id);
-//           // .from('items')
-//           // .select()
-//           // .match({ id: el });
-//           console.log(data)
-
-//         if (data) {
-//           setOwnerWants([
-
-//          data[0]
-
-//           ]);
-//         }
-//       });
-
-//     } catch (error) {
-//       console.error('try agina', error);
-//     } finally {
-//       setLoading(false);
-//     }
-
-//   };
-// console.log('items', ownerWantsItems)
 
 
-
-
-  // const [loading, setLoading] = useState(true);
-  // const [barter, setBarter] = useState([]);
-  // const user = supabase.auth.user();
-  // console.log('user', user.id);
-  // useEffect(() => {
-  //   buildBarter();
-  // }, [user.id]);
-  // const buildBarter = async () => {
-  //   try {
-  //     setLoading(true);
-  //     const { data, error } = await supabase
-  //       .from('barters')
-  //       .insert([{ userId: user.id }]);
-
-  //     console.log('getting', { ...data });
-  //     // const { data, error, status } = await supabase
-  //     //   .from('barters')
-  //     //   .select()
-  //     //   .match({ id: null });
-  //     if (data) {
-  //       setBarter(data);
-  //     }
-  //   } catch (error) {
-  //     console.error('try agina', error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-////
