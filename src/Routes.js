4942d557fe2fca84cf8c61ee5b10d1d15afa6a97
Colin Forbes <cjforbes32@gmail.com>
2022-03-22 11:04:16 -0400

    wrote the supabase query for fetching wishlist items.

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { supabase } from "./supabaseClient";
-import Auth from "./components/Auth";
-import Account from "./components/Account";
-import { Main, HaggleView, TradesAndMessages } from "./components";
-import NavBar from "./components/NavBar";
-import { AllItems } from "./components";
-import Listings from "./components/Listings";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
+import Auth from './components/Auth';
+import Account from './components/Account';
+import { Main, HaggleView, TradesAndMessages } from './components';
+import NavBar from './components/NavBar';
+import { AllItems } from './components';
+import Listings from './components/Listings';
 import {
   Routes,
   Route,
   Navigate,
   BrowserRouter as Router,
-} from "react-router-dom";
-import { useNavigate } from "react-router";
-import SingleItem from "./components/SingleItem";
+} from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import SingleItem from './components/SingleItem';
+import Wishlist from './components/Wishlist';
 
 const Routing = () => {
   const [session, setSession] = useState(null);
@@ -28,7 +29,7 @@ const Routing = () => {
     });
   }, []);
   return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <NavBar session={session} />
       {!session ? (
         <main>
@@ -38,6 +39,7 @@ const Routing = () => {
             <Route path="/" element={<Navigate to="/home" />} />
             <Route exact path="/items" element={<AllItems />} />
             <Route path="/items/:id" element={<SingleItem />} />
+            <Route path="/wishlist" element={<Wishlist />} />
           </Routes>
         </main>
       ) : (
@@ -54,6 +56,7 @@ const Routing = () => {
             <Route exact path="/haggle" element={<HaggleView />} />
             <Route exact path="/items" element={<AllItems />} />
             <Route path="/items/:id" element={<SingleItem />} />
+            <Route path="/wishlist" element={<Wishlist />} />
           </Routes>
         </main>
       )}
--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -3,9 +3,22 @@ import { supabase } from '../supabaseClient';
 import { useParams } from 'react-router-dom';
 
 const Wishlist = () => {
-  const [loading, setLoading] = useState;
-  const [wishlist, setWishList] = useState;
-  const params = useParams();
+  // const [loading, setLoading] = useState;
+  // const [wishlist, setWishList] = useState;
+  // const params = useParams();
+
+  useEffect(() => {
+    const getWishlist = async () => {
+      try {
+        // setLoading(true);
+        let { data, error, status } = await supabase
+          .from('wishlists')
+          .select(`*, items(name, description, image_url)`);
+        console.log(data);
+      } catch (error) {}
+    };
+    getWishlist();
+  }, []);
 
   return <div>Hello World</div>;
 };
