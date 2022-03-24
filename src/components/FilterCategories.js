1352ec807a1e102869d555bc438f5699202bc358
Brennan Skinner <bgskinner3@gmail.com>
2022-03-24 12:25:50 -0400

    added some more test data and begun to build out filter component

---
+++ src/components/FilterCategories.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+
+const FilterCategory = () => {
+    const [loading, setLoading] = useState(true);
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+      const filter = () => {
+        
+      }
+    })
+  return (
+    <div>
+      <div>filter </div>
+    </div>
+  );
+};
+
+export default FilterCategory;
