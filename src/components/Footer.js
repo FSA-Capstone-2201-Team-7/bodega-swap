63c80dc3f80e0713bf2354739be397fe60e3347a
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-24 13:47:03 -0400

    position at bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,23 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div></div>
-      <div></div>
-      <div></div>
+    <footer className="grid grid-cols-3 px-8 py-10 border-t-gray-400 border absolute bottom-0 w-full ">
+      <div>
+        <h2 className="text-lg font-bold">Bodega Swap</h2>
+      </div>
+      <div>
+        <ul className="flex space-x-4 text-gray-700 font-bold text-sm ">
+          <li>Mobile App</li>
+          <li>Community</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+      <div>
+        <p className="text-gray-500">
+          Copyright &copy; <span className="year">2022</span> Bodega Swap, Inc.
+          All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
