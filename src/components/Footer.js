dd2b9db725f451887ac10129a9dae2a1cfe916d3
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-24 12:56:38 -0400

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div></div>
+      <div></div>
+      <div></div>
+    </footer>
+  );
+}
+
+export default Footer;
