9a0b1427232fff988d5a26afb277536835bb12ee
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-24 15:03:07 -0400

    added more footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,22 +2,44 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="grid grid-cols-3 px-8 py-10 border-t-gray-400 border absolute bottom-0 left-0 right-0">
-      <div>
+    <footer className="grid grid-cols-3 px-8 pt-10 border-t-gray-400 border absolute bottom-0 left-0 right-0">
+      <div className="col-span-1">
         <h2 className="text-lg font-bold">Bodega Swap</h2>
       </div>
-      <div>
-        <ul className="flex space-x-4 text-gray-700 font-bold text-sm ">
+      <div className="flex col-span-2 justify-between">
+        <ul className="flex-cols  text-gray-700  text-sm ">
+          <h3 className="font-semibold pb-2">Shop</h3>
           <li>Mobile App</li>
-          <li>Community</li>
+          <li>Categories</li>
+          <li>Shop Local</li>
+        </ul>
+        <ul className="flex-cols text-gray-700  text-sm ">
+          <h3 className="font-semibold pb-2">Support</h3>
+          <li>Contact Us</li>
+          <li>Help Center</li>
+          <li>Trader Protection</li>
+          <li>Safty Guidelines</li>
+        </ul>
+        <ul className="flex-cols  text-gray-700  text-sm">
+          <h3 className="font-semibold pb-2">Policy Center</h3>
+          <li>Terms of Service</li>
+          <li>Privacy</li>
+          <li>Prohibited Items</li>
+          <li>Prohibited Conduct</li>
+        </ul>
+        <ul className="flex-cols  text-gray-700  text-sm">
+          <h3 className="font-semibold pb-2">Company</h3>
           <li>About Us</li>
+          <li>Blog</li>
+          <li>Careers</li>
         </ul>
       </div>
-      <div>
+      <div className=" justify-between">
         <p className="text-gray-500">
           Copyright &copy; <span className="year">2022</span> Bodega Swap, Inc.
           All rights reserved.
         </p>
+        <div className="soscial links"></div>
       </div>
     </footer>
   );
