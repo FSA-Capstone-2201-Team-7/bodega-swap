c1ff8e7b83e7e57d2a2a6fca2771d18f35ede947
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-29 16:09:39 -0400

    styled single item page

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
-import { useParams, useNavigate } from 'react-router-dom';
-import ToggleWishlistButton from './ToggleWishlistButton';
-import Card from './Card';
+import React, { useState, useEffect } from "react";
+import { supabase } from "../supabaseClient";
+import { useParams, useNavigate } from "react-router-dom";
+import ToggleWishlistButton from "./ToggleWishlistButton";
+import Card from "./Card";
 
 const SingleItem = () => {
   const params = useParams();
@@ -19,10 +19,10 @@ const SingleItem = () => {
     try {
       setLoading(true);
       let { data, error, status } = await supabase
-        .from('items')
-        .select(`*, users:ownerId(username)`)
-        .eq('id', params.id)
-        .neq('listed', false);
+        .from("items")
+        .select(`*, users:ownerId(username, avatarUrl)`)
+        .eq("id", params.id)
+        .neq("listed", false);
 
       if (error && status !== 406) {
         throw error;
@@ -35,6 +35,7 @@ const SingleItem = () => {
     } finally {
       setLoading(false);
     }
+    console.log("item", item);
   };
 
   return (
@@ -44,34 +45,55 @@ const SingleItem = () => {
       ) : !item ? (
         <div>This item is not currently listed, or no longer exists.</div>
       ) : (
-        <div className="single-item-container">
-          <Card
-            name={item.name}
-            id={item.id}
-            description={item.description}
-            imageUrl={item.image_url}
-            firstButton={
-              user && user.id !== item.ownerId ? (
-                <ToggleWishlistButton userId={user.id} itemId={item.id} />
-              ) : (
-                <></>
-              )
-            }
-            secondButton={
-              user ? (
+        <div className="flex-col flex gap-8 md:gap-36 md:flex-row mt-5 justify-center">
+          <div>
+            <img className=" shadow  h-96 w-96 " src={item.image_url} alt="" />
+
+            <div className="flex flex-col mt-5 pt-5 border-t-2">
+              <h4 className="text-xl font-medium	">Meet the Owner</h4>
+              {user ? (
                 <button
+                  className="pt-4 items-center  flex"
                   type="button"
-                  onClick={() => navigate('OwnerProfile', { state: { item } })}
+                  onClick={() => navigate("OwnerProfile", { state: { item } })}
                 >
-                  <p>Owner: {item.users.username}</p>
+                  <img
+                    className="w-16 h-16 rounded-full"
+                    src={item.users.avatarUrl}
+                    alt=""
+                  />
+                  <p className="pl-5 text-lg font-semibold">
+                    {" "}
+                    {item.users.username}
+                  </p>
                 </button>
               ) : (
                 <h3>
                   <p>Owner: {item.users.username}</p>
                 </h3>
-              )
-            }
-          />
+              )}
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="text-3xl font-semibold mb-6">{item.name}</p>
+            <p className="font-semibold text-lg mb-3">Desciption</p>
+            <p>{item.description}</p>
+            {user && user.id !== item.ownerId ? (
+              <div className="flex justify-end my-2">
+                <ToggleWishlistButton userId={user.id} itemId={item.id} />
+              </div>
+            ) : (
+              <></>
+            )}
+            <button
+              type="button"
+              className="bg-indigo-600 mb-2 hover:bg-indigo-500 text-white font-bold py-2 px-4 mt-2 rounded-md"
+              onClick={() => navigate("/createproposal", { state: { item } })}
+            >
+              Propose Trade
+            </button>
+          </div>
         </div>
       )}
     </div>
