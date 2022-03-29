63414f55642864ffe7543a2d8a08fa0012470540
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-29 03:11:37 -0400

    more css

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -63,29 +63,25 @@ const Wishlist = () => {
         </h3>
         {wishlist.map((item, idx) => {
           return (
-            <div
-              key={idx}
-              className=" max-w-sm card card-compact w-96 bg-base-100 shadow-xl"
-            >
+            <div key={idx} className="rounded overflow-hidden shadow-xl">
               <Link to={`/items/${item.items.id}`}>
                 <img className="h-80 w-96" src={item.items.image_url} alt="" />
               </Link>
-              <div className="card-body">
+              <div className="card-body flex flex-row justify-between">
                 <p className="card-title">{item.items.name}</p>
-              </div>
-              <div className="card-actions justify-end">
                 <Link to="/createproposal" state={{ item }}>
                   <button
-                    className="cursor-pointer rounded-lg bg-indigo-600 px-4 py-2 text-sm text-white w-full hover:bg-indigo-500"
+                    className="cursor-pointer rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white w-full hover:bg-indigo-500"
                     type="button"
                   >
                     Haggle
                   </button>
                 </Link>
               </div>
+
               <button
                 type="button"
-                className="cursor-pointer mt-5 rounded-lg bg-indigo-600 px-4 py-2 text-sm text-white w-full hover:bg-red-500"
+                className="cursor-pointer rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white w-full hover:bg-red-500"
                 onClick={(e) => handleRemove(e, item.items.id)}
               >
                 Remove From Wishlist
