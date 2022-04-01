5dad483697c95e00b8f27bf19e07c5bc621104e8
Colin Forbes <cjforbes32@gmail.com>
2022-04-01 12:47:00 -0400

    wrote code to create an account based on demo user data

--- src/components/DemoAccount.js
+++ src/components/DemoAccount.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { supabase } from '../supabaseClient'
+import LoadingPage from './LoadingPage'
 
 const DemoAccount = () => {
+  let demoUser
+  const [demoPassword, setDemoPassword] = useState(null)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    setDemoPassword(`demo${randomInt()}`)
+  }, [])
+
   const randomInt = () => {
     return Math.floor(Math.random() * (100000))
   }
   const createDemoAccount = async (e) => {
     e.preventDefault()
     try {
-      let { user, error } = await supabase.auth.signUp({ email: `demoaccount${randomInt()}@demoacct.com`, password: `demo${randomInt()}` })
+      let { user, error } = await supabase.auth.signUp({ email: `demoaccount${randomInt()}@demoacct.com`, password: [demoPassword] })
       if (error) throw error
-      console.log(user)
+      demoUser = user
     } catch (error) {
       console.error(error)
+    } finally {
+      try {
+        console.log(demoUser)
+        let { data, error } = await supabase.from('users').upsert({
+          id: demoUser.id,
+          username: `demoUser${demoPassword}`
+        })
+        if (error) throw error
+      } catch (error) {
+        console.error(error)
+      } finally {
+        await supabase.auth.signIn({
+          email: demoUser.email,
+          password: demoPassword
+        })
+      }
     }
   }
 
   return (
+    !demoPassword ? 
     <button type='button' onClick={createDemoAccount}>Demo Account</button>
-  )
+ : <LoadingPage /> )
 } 
 
 export default DemoAccount
